5a1a7b688c877ba992fd570a3af16ddef37b7687
Bhavya <example@gmail.com>
2013-07-10 18:53:41 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
-
-var express = require("express");
-
-var app = express.createServer(express.logger());
- app.get('/', function(request, response) {
-      var fs = require("fs");
-            var content = fs.readFileSync('index.html', 'utf8');          
- response.send(buf.toString());
+var express = require('express');
+var  fs = require('fs');
+ var app = express.createServer(express.logger());
+ var buf = fs.readFileSync('index.html', 'utf-8');
+  var string = buf.toString(); 
+app.get('/', function(request, response) {
+      
+                           response.send(string);
 });
 
 var port = process.env.PORT || 5000;
